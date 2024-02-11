69e15927d56db35e9df2c33219fcef618d8dc6f2
Ihor Serhiienko <igorserhiyenko@gmail.com>
2024-02-11 20:14:02 +0200

    feat: add get user handler

--- app.ts
+++ app.ts
@@ -1,19 +1,30 @@
-import http from 'http';
-import dotenv from 'dotenv';
+import http, { IncomingMessage, ServerResponse } from "http";
+import dotenv from "dotenv";
+import { handleGET } from "./handlers/getUserHandler";
 
 dotenv.config();
 
-const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse): void => {
-
-});
+const server: http.Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse): void => {
+    if (req.method === "GET") {
+      const url = req.url;
+      handleGET(req, res, url);
+    } else {
+      res.writeHead(405, { "Content-Type": "text/plain" });
+      res.end("Method Not Allowed");
+    }
+  },
+);
 
 const PORT: string | undefined = process.env.PORT;
 
 if (PORT) {
-    server.listen(parseInt(PORT, 10), () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
+  server.listen(parseInt(PORT, 10), () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 } else {
-    console.log('Error while reading .env occurred');
-    console.log('.env should contain the number of a port in the format PORT={number}');
+  console.log("Error while reading .env occurred");
+  console.log(
+    ".env should contain the number of a port in the format PORT={number}",
+  );
 }
