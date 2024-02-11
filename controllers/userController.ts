45a58e10450876ce706f00aefa99b02fb68c7498
Ihor Serhiienko <igorserhiyenko@gmail.com>
2024-02-11 19:07:57 +0200

    feat: add user controller

---
+++ controllers/userController.ts
@@ -0,0 +1,22 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export let users: {id: string, username: string, age: number, hobbies: string[]}[] = [
+    {
+        id: uuidv4(),
+        username: 'user1',
+        age: 20,
+        hobbies: ['sport', 'music']
+    },
+    {
+        id: uuidv4(),
+        username: 'user2',
+        age: 30,
+        hobbies: ['cars', 'girls']
+    },
+    {
+        id: uuidv4(),
+        username: 'user3',
+        age: 40,
+        hobbies: ['food', 'nature']
+    }
+];
